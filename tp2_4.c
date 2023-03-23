12da997672aa61f54d018bc45d1e2a6e609bc472
LeanCas <leandrocasavalle10@gmail.com>
2023-03-23 19:50:04 -0300

    punto 4 incompleto en C

---
+++ tp2_4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+struct compu {
+
+    int velocidad;//entre 1 y 3 GHz
+
+    int anio;//entre 2015 y 2023
+
+    int cantidad;//entre 1 y 8
+
+    char *tipo_cpu; //valores del arreglo tipos
+
+};
+
+
+
+
+
+int main (){
+
+    char tipos[6][10]={"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
+
+    struct compu stock[5]; 
+
+
+
+    return 0;
+}
